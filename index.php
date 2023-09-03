b73df89bd44be660a3cffc8b74a60e6488bcaafe
MmmNaad <125236702+sicmnvio@users.noreply.github.com>
2023-09-03 06:24:57 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+﻿<?php
+	if (!file_exists('madeline.php')) {
+ copy('https://phar.madelineproto.xyz/madeline.php','madeline.php');
+}
+ini_set('error_logs','off');
+error_reporting(0);
+unlink('MadelineProto.log');
+define('MADELINE_BRANCH', 'deprecated');
+//End
+//include MadeLineProto :
+include 'madeline.php';
+//End
+//Functions :
+	function closeConnection($message = 'Bot Is Running...')
+{
+    if (php_sapi_name() === 'cli' || isset($GLOBALS['exited'])) {
+        return;
+    }
+    @ob_end_clean();
+    header('Connection: close');
+    ignore_user_abort(true);
+    ob_start();
+    echo '<html><body><h1>'.$message.'</h1></body</html>';
+    $size = ob_get_length();
+    header("Content-Length: $size");
+    header('Content-Type: text/html');
+    ob_end_flush();
+    flush();
+    $GLOBALS['exited'] = true;
+}
+function shutdown_function($lock)
+{
+    $a = fsockopen((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] ? 'tls' : 'tcp').'://'.$_SERVER['SERVER_NAME'], $_SERVER['SERVER_PORT']);
+    fwrite($a, $_SERVER['REQUEST_METHOD'].' '.$_SERVER['REQUEST_URI'].' '.$_SERVER['SERVER_PROTOCOL']."\r\n".'Host: '.$_SERVER['SERVER_NAME']."\r\n\r\n");
+    flock($lock, LOCK_UN);
+    fclose($lock);
+}
+  
+//EndFunctions
+//closeConnection :
+if (!file_exists('bot.lock')) {
+    touch('bot.lock');
+}
+$lock = fopen('bot.lock', 'r+');
+
+$try = 1;
+$locked = false;
+while (!$locked) {
+    $locked = flock($lock, LOCK_EX | LOCK_NB);
+    if (!$locked) {
+        closeConnection();
+
+        if ($try++ >= 30) {
+            exit;
+        }
+        sleep(1);
+    }
+}
+//End 
+//StartMadeLine :
+$MadelineProto = new \danog\MadelineProto\API('session.madeline');
+$MadelineProto->start();
+$offset = 0;
+
+register_shutdown_function('shutdown_function', $lock);
+closeConnection();
+
+while (true) {
+    $updates = $MadelineProto->get_updates(['offset' => $offset, 'limit' => 50, 'timeout' => 0]); // Just like in the bot API, you can specify an offset, a limit and a timeout
+    \danog\MadelineProto\Logger::log($updates);
+    foreach ($updates as $update) {
+        $offset = $update['update_id'] + 1; // Just like in the bot API, the offset must be set to the last update_id
+     $up = $update['update']['_'];
+                if ($up == 'updateNewMessage' or $up == 'updateNewChannelMessage') {
+                if (isset($update['update']['message']['out']) && $update['update']['message']['out']) {
+                    continue;
+                }
+               $chatID = $MadelineProto->get_info($update['update']);
+               $type = $chatID['type'];
+               $chatID = $chatID['bot_api_id'];
+               $userID = $update['update']['message']['from_id'];
+               $msg = $update['update']['message']['message'];
+               $msg_id = $update['update']['message']['id'];
+                try {
+   
+  require 'plugin.php';
+                
+                } catch (\danog\MadelineProto\RPCErrorException $e) {
+                    $MadelineProto->messages->sendMessage(['peer' => [*[ADMIN]*], 'message' => $e->getCode().': '.$e->getMessage().PHP_EOL.$e->getTraceAsString()]);
+                }
+         
+        }
+    
+}
+}
---
+++ plugin.php
@@ -0,0 +1,27 @@
+<?php
+                 $admins = [
+  77777, //ایدی عددی
+];
+$listplugins = [
+  "ping",
+  "help",
+  "option",
+  "sendads",
+  "countchats",
+  "addall",
+  "link",
+]; //========================
+$cplug = count($listplugins) - 1;
+for($n=0; $n<=$cplug; $n++) {
+  $pluginlist = "plugins/".$listplugins[$n].".php";
+  include($pluginlist);
+}
+$data = json_decode(file_get_contents("data.json"),true);
+$markread = $data["data"]["markread"];
+$typing = $data["data"]["typing"];
+$contacts = $data["data"]["contacts"];
+$link = $data["data"]["link"];
+unlink("MadelineProto.log");
+
+
+
