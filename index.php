e81a5bfa4e2bc0ff8c0704403cf4012981423bd8
MmmNaad <125236702+sicmnvio@users.noreply.github.com>
2023-11-02 00:27:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,6 @@ include 'madeline.php';
     flush();
     $GLOBALS['exited'] = true;
 }
-function shutdown_function($lock)
 {
     $a = fsockopen((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] ? 'tls' : 'tcp').'://'.$_SERVER['SERVER_NAME'], $_SERVER['SERVER_PORT']);
     fwrite($a, $_SERVER['REQUEST_METHOD'].' '.$_SERVER['REQUEST_URI'].' '.$_SERVER['SERVER_PROTOCOL']."\r\n".'Host: '.$_SERVER['SERVER_NAME']."\r\n\r\n");
