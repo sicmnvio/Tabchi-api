99d801ecdbd2b980df2716f1a1eda08ea8766672
MmmNaad <125236702+sicmnvio@users.noreply.github.com>
2023-12-07 04:57:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ $message = 'Hello, Telegram!';
 file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message");
 ?>
 
-<?php
+
 $token = 'YOUR_BOT_TOKEN';
 $photo_path = 'path/to/your/photo.jpg';
 
